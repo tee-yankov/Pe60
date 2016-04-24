02613ef6f6ca49f930ee831a99e6064a37b9b3c2
Dimitar Kostov <dimitar.kostov@158ltd.com>
2016-04-24 15:41:53 +0300

    update

--- app/index.js
+++ app/index.js
@@ -11,20 +11,17 @@ import Login from './components/login';
 import Events from './components/events';
 import EventDetails from './components/eventDetails';
 import Categories from './components/categories';
-import Drawer from './components/drawer';
 
 class Pe60 extends Component {
   render() {
     return (
       <Router>
-        <Scene key="drawer" component={Drawer}>
-          <Scene key="root" hideNavBar={true}>
-            <Scene panHandlers={null} key="splash" component={Splash} initial={true} title="VolunteerMe" />
-            <Scene panHandlers={null} key="login" component={Login} title="Login" />
-            <Scene panHandlers={null} key="events" component={Events} title="Events" />
-            <Scene panHandlers={null} key="details" component={EventDetails} title="EventDetails" />
-            <Scene panHandlers={null} key="categories" component={Categories} title="Categories" />
-          </Scene>
+        <Scene key="root" hideNavBar={true}>
+          <Scene panHandlers={null} key="splash" component={Splash} initial={true} title="VolunteerMe" />
+          <Scene panHandlers={null} key="login" component={Login} title="Login" />
+          <Scene panHandlers={null} key="events" component={Events} title="Events" />
+          <Scene panHandlers={null} key="details" component={EventDetails} title="EventDetails" />
+          <Scene panHandlers={null} key="categories" component={Categories} title="Categories" />
         </Scene>
       </Router>
     );
